00ab797c266e62528c385b3c19e9f5d38a57dd23
jonaspiterek <piterek@kitconcept.com>
2024-01-12 10:30:02 +0100

    add basic test

--- acceptance/cypress/tests/basic.cy.js
+++ acceptance/cypress/tests/basic.cy.js
@@ -1,8 +1,3 @@
-import {
-  getSlateEditorAndType,
-  getSelectedSlateEditor,
-} from '../support/slate';
-
 context('Basic Acceptance Tests', () => {
   describe('Text Block Tests', () => {
     beforeEach(() => {
@@ -12,6 +7,11 @@ context('Basic Acceptance Tests', () => {
       // given a logged in editor and a page in edit mode
       cy.autologin();
       cy.createContent({
+        contentType: 'Image',
+        contentId: 'image',
+        contentTitle: 'my-image',
+      });
+      cy.createContent({
         contentType: 'Document',
         contentId: 'document',
         contentTitle: 'Document',
@@ -20,8 +20,8 @@ context('Basic Acceptance Tests', () => {
       cy.wait('@content');
     });
 
-    it('As editor I can add a page with a text block', function () {
-      // when I add a page with a text block
+    it('As editor I can add a page with a testimonials block with multiple slides', function () {
+      // when I add a page with a testimonials block with multiple slides
       cy.get('#toolbar-add').click();
       cy.get('#toolbar-add-document').click();
       cy.get('.documentFirstHeading')
@@ -29,43 +29,61 @@ context('Basic Acceptance Tests', () => {
         .get('.documentFirstHeading')
         .contains('My Page');
 
-      getSlateEditorAndType(
-        '.block .slate-editor [contenteditable=true]',
-        'This is the text',
-      );
-
-      getSelectedSlateEditor().contains('This is the text');
-      cy.get('#toolbar-save').click();
+      cy.get('[aria-multiline="false"] > p').click();
+      cy.get('.text-slate-editor-inner > .ui > .icon').click();
+      cy.get(
+        '[style="transition: opacity 500ms ease 0ms;"] > :nth-child(6) > .ui > .icon'
+      ).click();
+      // slide 0
+      cy.get('#field-testimonial-0-slides-0')
+        .click()
+        .type(
+          'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+        );
+      cy.get(
+        '.field-wrapper-image-1-slides-0 > .grid > .stretched > .eight > .objectbrowser-field > .ui > .icon'
+      ).click();
+      cy.get('.secondary > .ui > :nth-child(1) > .icon').click();
+      cy.get('[title="/image (Image)"]').dblclick();
+      cy.get('#field-name-2-slides-0').click().type('Lorem Ipsum');
+      cy.get('#field-additionalData-3-slides-0')
+        .click()
+        .type('Lorem Ipsum dolor sit amet');
+      // slide 1
+      cy.get('.add-item-button-wrapper > .ui').click();
+      cy.get('#field-testimonial-0-slides-1')
+        .click()
+        .type(
+          'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+        );
+      cy.get(
+        '.field-wrapper-image-1-slides-1 > .grid > .stretched > .eight > .objectbrowser-field > .ui > .icon'
+      ).click();
+      cy.get('.secondary > .ui > :nth-child(1) > .icon').click();
+      cy.get('[title="/image (Image)"]').dblclick();
+      cy.get('#field-name-2-slides-1').click().type('Lorem Ipsum');
+      cy.get('#field-additionalData-3-slides-1')
+        .click()
+        .type('Lorem Ipsum dolor sit amet');
+      // slide 2
+      cy.get('.add-item-button-wrapper > .ui').click();
+      cy.get('#field-testimonial-0-slides-2')
+        .click()
+        .type(
+          'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+        );
+      cy.get(
+        '.field-wrapper-image-1-slides-2 > .grid > .stretched > .eight > .objectbrowser-field > .ui > .icon'
+      ).click();
+      cy.get('.secondary > .ui > :nth-child(1) > .icon').click();
+      cy.get('[title="/image (Image)"]').dblclick();
+      cy.get('#field-name-2-slides-2').click().type('Lorem Ipsum');
+      cy.get('#field-additionalData-3-slides-2')
+        .click()
+        .type('Lorem Ipsum dolor sit amet');
+      cy.get('#toolbar-save > .icon').click();
       cy.wait('@content');
       cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
-    });
-
-    it('As editor I can add a link to a text block', function () {
-      cy.navigate('/document/edit');
-      cy.wait('@schema');
-
-      cy.get('.block.inner.title .documentFirstHeading');
-
-      cy.log('when I create a link');
-
-      cy.getSlateEditorAndType(
-        'Colorless green ideas sleep furiously.',
-      ).setSlateSelection('furiously');
-      cy.clickSlateButton('Add link');
-      cy.get('.slate-toolbar .link-form-container input').type(
-        'https://google.com{enter}',
-      );
-
-      cy.get('#toolbar-save', { timeout: 10000 }).click();
-      cy.wait('@content');
-
-      cy.log('then the page view should contain a link');
-
-      cy.get('.container p').contains('Colorless green ideas sleep furiously.');
-      cy.get('.container p a')
-        .should('have.text', 'furiously')
-        .and('have.attr', 'href')
-        .and('include', 'https://google.com');
     });
   });
 });
